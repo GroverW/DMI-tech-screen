e40c5ca666198246e1ab21d86a0bfe9ae7e52254
GroverW <cheesemaker@gmail.com>
2020-05-08 18:50:27 -0700

    added config file and utils to get page num

---
+++ server/config.js
@@ -0,0 +1,7 @@
+/** Shared config for application. */
+
+const RESULTS_PER_PAGE = 25;
+
+module.exports = {
+  RESULTS_PER_PAGE,
+};
---
+++ server/utils/helpers.js
@@ -0,0 +1,10 @@
+const { RESULTS_PER_PAGE } = require('../config');
+
+const getPageStart = page =>
+  page && Number.isInteger(+page)
+    ? Math.abs(Math.max(page - 1, 0) * RESULTS_PER_PAGE)
+    : 0;
+
+module.exports = {
+  getPageStart,
+};
