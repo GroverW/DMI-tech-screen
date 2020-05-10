10e791479ed14767ca86792a546fa570e17adc20
GroverW <cheesemaker@gmail.com>
2020-05-09 20:53:16 -0700

    added addButton message to NewStringForm

--- app/components/NewStringForm/messages.js
+++ app/components/NewStringForm/messages.js
@@ -9,8 +9,8 @@ import { defineMessages } from 'react-intl';
 export const scope = 'app.components.NewStringForm';
 
 export default defineMessages({
-  header: {
-    id: `${scope}.header`,
-    defaultMessage: 'This is the NewStringForm component!',
+  addButton: {
+    id: `${scope}.addButton`,
+    defaultMessage: 'Add String',
   },
 });
