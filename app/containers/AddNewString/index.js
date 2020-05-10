752ec5dc18393dfc45f98031c14cdda355584096
GroverW <cheesemaker@gmail.com>
2020-05-09 20:54:11 -0700

    added props to AddNewString container

--- app/containers/AddNewString/index.js
+++ app/containers/AddNewString/index.js
@@ -9,15 +9,19 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
-import { compose } from 'redux';
 
+import { createSelector } from 'reselect';
 import { useInjectSaga } from 'utils/injectSaga';
 import saga from './saga';
 import messages from './messages';
+import { addString } from './actions';
+import { makeSelectStrings } from './selectors';
 
-export function AddNewString() {
+import NewStringForm from '../../components/NewStringForm';
+
+export function AddNewString(props) {
   useInjectSaga({ key: 'addNewString', saga });
-
+  console.log(props);
   return (
     <div>
       <Helmet>
@@ -25,23 +29,31 @@ export function AddNewString() {
         <meta name="description" content="Description of AddNewString" />
       </Helmet>
       <FormattedMessage {...messages.header} />
+      <NewStringForm addString={props.addString} />
     </div>
   );
 }
 
 AddNewString.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  strings: PropTypes.object,
+  addString: PropTypes.func,
 };
 
-function mapDispatchToProps(dispatch) {
+const mapStateToProps = createSelector(
+  makeSelectStrings(),
+  strings => ({
+    strings,
+  }),
+);
+
+export function mapDispatchToProps(dispatch) {
   return {
+    addString: data => dispatch(addString(data)),
     dispatch,
   };
 }
 
-const withConnect = connect(
-  null,
+export default connect(
+  mapStateToProps,
   mapDispatchToProps,
-);
-
-export default compose(withConnect)(AddNewString);
+)(AddNewString);
