24e23fa16a19a1bf1d083aa640d6010b9c475088
GroverW <cheesemaker@gmail.com>
2020-05-09 21:02:12 -0700

    refactored App container urls and added theme prop

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,26 +9,46 @@
 
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import HomePage from 'containers/HomePage/Loadable';
 import AddNewString from 'containers/AddNewString';
 import AllStrings from 'containers/AllStrings';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Navbar from '../../components/Navbar';
+import { makeSelectSetTheme } from '../SetThemeProvider/selectors';
+import Header from '../../components/Header';
 
 import GlobalStyle from '../../global-styles';
 
-export default function App() {
+import { ALL_STRINGS, ADD_STRING } from './urls';
+
+function App(props) {
   return (
     <div>
-      <Navbar />
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/strings" component={AllStrings} />
-        <Route exact path="/strings/new" component={AddNewString} />
+        <Route exact path={ALL_STRINGS} component={AllStrings} />
+        <Route exact path={ADD_STRING} component={AddNewString} />
         <Route component={NotFoundPage} />
       </Switch>
-      <GlobalStyle />
+      <GlobalStyle theme={props.theme} />
     </div>
   );
 }
+
+App.propTypes = {
+  theme: PropTypes.object,
+};
+
+const mapStateToProps = createSelector(
+  makeSelectSetTheme(),
+  theme => ({
+    theme: theme.colors,
+  }),
+);
+
+export default connect(mapStateToProps)(App);
