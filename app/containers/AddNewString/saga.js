88c87cd3ec510b468769133e0cf0d19715c88c12
GroverW <cheesemaker@gmail.com>
2020-05-09 20:54:38 -0700

    added AddNewString saga [WIP]

--- app/containers/AddNewString/saga.js
+++ app/containers/AddNewString/saga.js
@@ -1,6 +1,22 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+import { ADD_STRING_URL } from './urls';
+import { addStringSuccess, addStringError } from './actions';
+import { ADD_STRING } from './constants';
 
+export function* postNewString(action) {
+  try {
+    const { data } = action;
+    const response = yield call(() => axios.post(ADD_STRING_URL, { data }));
+
+    yield put(addStringSuccess(response.data.string));
+  } catch (error) {
+    yield put(addStringError(error));
+  }
+}
 // Individual exports for testing
 export default function* addNewStringSaga() {
+  console.log('CAUGHT YA!');
   // See example in containers/HomePage/saga.js
+  yield takeLatest(ADD_STRING, postNewString);
 }
