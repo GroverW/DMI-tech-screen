d7e721af46ba571e935483a58ea3baaaa97ede47
GroverW <cheesemaker@gmail.com>
2020-05-09 21:04:30 -0700

    added theme and string reducers to main reducers

--- app/reducers.js
+++ app/reducers.js
@@ -7,6 +7,8 @@ import { connectRouter } from 'connected-react-router';
 
 import history from 'utils/history';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
+import setThemeProviderReducer from 'containers/SetThemeProvider/reducer';
+import stringsReducer from 'containers/AddNewString/reducer';
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
@@ -14,6 +16,8 @@ import languageProviderReducer from 'containers/LanguageProvider/reducer';
 export default function createReducer(injectedReducers = {}) {
   const rootReducer = combineReducers({
     language: languageProviderReducer,
+    theme: setThemeProviderReducer,
+    strings: stringsReducer,
     router: connectRouter(history),
     ...injectedReducers,
   });
