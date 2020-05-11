be58f2d0674129ac4976c5200c2cf748e1047fa0
GroverW <cheesemaker@gmail.com>
2020-05-10 21:11:43 -0700

    added ContentContainer component

---
+++ app/components/ContentContainer/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ContentContainer
+ *
+ */
+
+import styled from 'styled-components';
+
+const ContentContainer = styled.div`
+  padding: 10px 15px;
+  margin: 0px auto;
+  width: 600px;
+  box-shadow: 0px 4px 2px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+`;
+
+export default ContentContainer;
