8113559f9d99df783b06537b0b50bf6ef30000ef
GroverW <cheesemaker@gmail.com>
2020-05-10 21:39:49 -0700

    added axios and updtaed shelljs

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -2,7 +2,7 @@ const { RESULTS_PER_PAGE } = require('../config');
 
 const getPageStart = page =>
   page && Number.isInteger(+page)
-    ? Math.abs(Math.max(page - 1, 0) * RESULTS_PER_PAGE)
+    ? Math.max(page - 1, 0) * RESULTS_PER_PAGE
     : 0;
 
 module.exports = {
