9b7142514c239c0a335efb6ebf69e32e116fce73
GroverW <cheesemaker@gmail.com>
2020-05-10 21:26:11 -0700

    added InputRow component to NewStringForm

---
+++ app/components/NewStringForm/InputRow.js
@@ -0,0 +1,14 @@
+/**
+ * InputRow
+ */
+
+import styled from 'styled-components';
+
+const InputRow = styled.div`
+  margin: 10px 0px;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+export default InputRow;
