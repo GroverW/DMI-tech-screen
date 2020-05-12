67b5015ad7f35ab4c1b4c757ffd07f6a83e00157
GroverW <cheesemaker@gmail.com>
2020-05-12 13:04:44 -0700

    removed unecessary formatting helper in getErrors

--- app/utils/getErrors.js
+++ app/utils/getErrors.js
@@ -1,11 +1,15 @@
+/* 
+get first digit of statusCode
+*/
 const statusCategory = statusCode => Math.floor(statusCode / 100);
 
-const formatErrors = errorsArray =>
-  errorsArray.map(error => Object.values(error)[0]);
-
+/*
+return array of errors if status code in 400s
+else received server error and return default message
+*/
 const getErrors = error =>
   statusCategory(error.status) === 4
-    ? formatErrors(error.data.message)
+    ? error.data.message
     : ['Something went wrong. Please try again later.'];
 
 export default getErrors;
