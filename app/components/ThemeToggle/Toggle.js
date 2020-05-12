d91d2bc18bb289e5803097b4963002c6d63b8222
GroverW <cheesemaker@gmail.com>
2020-05-12 09:02:45 -0700

    updated border style and added cursor on hover to ThemeToggle

--- app/components/ThemeToggle/Toggle.js
+++ app/components/ThemeToggle/Toggle.js
@@ -7,7 +7,11 @@ import styled from 'styled-components';
 const Toggle = styled.select`
   background-color: ${props => props.theme.background};
   color: ${props => props.theme.font};
-  border: 1px solid ${props => props.theme.border};
+  border: 1px solid ${props => props.theme.buttonBorder};
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default Toggle;
