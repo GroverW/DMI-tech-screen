5bde3b7c1d233bf3f73a7cf41d3023f4e307591c
GroverW <cheesemaker@gmail.com>
2020-05-12 12:58:27 -0700

    added NewStringForm and Navbar tests

--- app/components/Navbar/tests/index.test.js
+++ app/components/Navbar/tests/index.test.js
@@ -7,40 +7,44 @@
  */
 
 import React from 'react';
-import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
+import configureStore from '../../../configureStore';
+import { renderWithRouter } from '../../../tests/helpers/testHelper';
 // import 'jest-dom/extend-expect'; // add some helpful assertions
 
 import Navbar from '../index';
 import { DEFAULT_LOCALE } from '../../../i18n';
 
 describe('<Navbar />', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
-    render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <Navbar />
-      </IntlProvider>,
+    renderWithRouter(
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <Navbar />
+        </IntlProvider>
+      </Provider>,
     );
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('Expect to have additional unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-
-  /**
-   * Unskip this test to use it
-   *
-   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
-   */
-  it.skip('Should render and match the snapshot', () => {
+  it('Should render and match the snapshot', () => {
     const {
       container: { firstChild },
-    } = render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <Navbar />
-      </IntlProvider>,
+    } = renderWithRouter(
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <Navbar />
+        </IntlProvider>
+      </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
   });
