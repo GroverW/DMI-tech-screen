dc800190f265c71c05aae22d0828e14128b00c9b
GroverW <cheesemaker@gmail.com>
2020-05-12 08:38:07 -0700

    added error handling to AddNewString saga

--- app/containers/Strings/AddNewString/saga.js
+++ app/containers/Strings/AddNewString/saga.js
@@ -1,5 +1,6 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
+import getErrors from 'utils/getErrors';
 import { ADD_STRING_URL } from '../urls';
 import { addStringSuccess, addStringError } from './actions';
 import { ADD_STRING } from '../constants';
@@ -7,11 +8,12 @@ import { ADD_STRING } from '../constants';
 export function* postNewString(action) {
   try {
     const { data } = action;
+
     const response = yield call(() => axios.post(ADD_STRING_URL, { data }));
 
     yield put(addStringSuccess(response.data.string));
   } catch (error) {
-    yield put(addStringError(error.response.data.error));
+    yield put(addStringError(getErrors(error.response)));
   }
 }
 // Individual exports for testing
