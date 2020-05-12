8a5424f608cf8e121174a88d9310cb8cad8d5751
GroverW <cheesemaker@gmail.com>
2020-05-12 09:10:21 -0700

    added fontawesome imports to app

--- app/app.js
+++ app/app.js
@@ -28,6 +28,10 @@ import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import 'file-loader?name=.htaccess!./.htaccess';
 /* eslint-enable import/no-unresolved, import/extensions */
 
+// Import fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+
 import configureStore from './configureStore';
 
 // Import i18n messages
@@ -81,3 +85,5 @@ if (!window.Intl) {
 if (process.env.NODE_ENV === 'production') {
   require('offline-plugin/runtime').install(); // eslint-disable-line global-require
 }
+
+library.add(faCog);
