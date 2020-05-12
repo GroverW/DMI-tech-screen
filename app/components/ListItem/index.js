bd63b3bee92ac86ca709a7e38286743a34c6f62d
GroverW <cheesemaker@gmail.com>
2020-05-12 09:06:40 -0700

    updated styling for ListItem

--- app/components/ListItem/index.js
+++ app/components/ListItem/index.js
@@ -7,8 +7,8 @@ import styled from 'styled-components';
 const ListItem = styled.div`
   padding: 20px 15px;
   margin: 15px auto;
-  width: 600px;
-  border-left: 3px solid ${props => props.theme.border};
+  width: 100%;
+  border-left: 3px solid ${props => props.theme.buttonBorder};
   text-align: left;
   background-color: ${props => props.theme.backgroundSubtle};
   border-radius: 0 5px 5px 0;
