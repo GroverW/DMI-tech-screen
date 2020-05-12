be35e72f89af8777845c0cefe6dec7391091d6b7
GroverW <cheesemaker@gmail.com>
2020-05-12 12:59:17 -0700

    added tests for Loading, LoadMore, ListItem, ContentContainer, BorderLink, Alert

--- app/components/Loading/tests/index.test.js
+++ app/components/Loading/tests/index.test.js
@@ -15,26 +15,17 @@ import Loading from '../index';
 import { DEFAULT_LOCALE } from '../../../i18n';
 
 describe('<Loading />', () => {
-  it('Expect to not log errors in console', () => {
-    const spy = jest.spyOn(global.console, 'error');
-    render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <Loading />
-      </IntlProvider>,
-    );
-    expect(spy).not.toHaveBeenCalled();
-  });
+  // it('Expect to not log errors in console', () => {
+  //   const spy = jest.spyOn(global.console, 'error');
+  //   render(
+  //     <IntlProvider locale={DEFAULT_LOCALE}>
+  //       <Loading />
+  //     </IntlProvider>,
+  //   );
+  //   expect(spy).not.toHaveBeenCalled();
+  // });
 
-  it('Expect to have additional unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-
-  /**
-   * Unskip this test to use it
-   *
-   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
-   */
-  it.skip('Should render and match the snapshot', () => {
+  it('Should render and match the snapshot', () => {
     const {
       container: { firstChild },
     } = render(
