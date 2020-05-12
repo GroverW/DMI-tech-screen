32c03db927747c09353c5560fd8ddab8578a8541
GroverW <cheesemaker@gmail.com>
2020-05-12 13:08:15 -0700

    added docstring to utils helper

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,3 +1,7 @@
+/* 
+helper function used to pause app an amount of ms.
+used so loading spinner will display in test.
+*/
 const sleep = ms =>
   new Promise(resolve => {
     setTimeout(resolve, ms);
