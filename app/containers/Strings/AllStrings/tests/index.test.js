78e5cb8e0590681050927619844670ace6d1a199
GroverW <cheesemaker@gmail.com>
2020-05-12 12:54:01 -0700

    added AllStrings tests

--- app/containers/Strings/AllStrings/tests/index.test.js
+++ app/containers/Strings/AllStrings/tests/index.test.js
@@ -9,40 +9,104 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
-// import 'jest-dom/extend-expect'; // add some helpful assertions
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
+import configureStore from '../../../../configureStore';
+import 'jest-dom/extend-expect'; // add some helpful assertions
+import { loadStrings } from '../actions';
 
-import { AllStrings } from '../index';
+import { AllStrings, mapDispatchToProps } from '../index';
 import { DEFAULT_LOCALE } from '../../../../i18n';
 
 describe('<AllStrings />', () => {
+  let store;
+  let mockTheme;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+
+    mockTheme = {
+      name: '',
+      colors: {},
+    };
+  });
+
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
-    const dispatch = jest.fn();
     render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <AllStrings dispatch={dispatch} />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <AllStrings
+            strings={[]}
+            loading={false}
+            errors={false}
+            loaded={false}
+            theme={mockTheme}
+            loadStrings={loadStrings}
+          />
+        </IntlProvider>
+      </Provider>,
     );
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('Expect to have additional unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-
-  /**
-   * Unskip this test to use it
-   *
-   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
-   */
-  it.skip('Should render and match the snapshot', () => {
+  it('Should render and match the snapshot', () => {
     const {
       container: { firstChild },
     } = render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <AllStrings />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <AllStrings
+            strings={[]}
+            loading={false}
+            errors={false}
+            loaded={false}
+            theme={mockTheme}
+            loadStrings={loadStrings}
+          />
+        </IntlProvider>
+      </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
+  });
+
+  it('Should load the body content', async () => {
+    const { queryByText } = render(
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <AllStrings
+            strings={[]}
+            loading={false}
+            errors={false}
+            loaded={false}
+            theme={mockTheme}
+            loadStrings={loadStrings}
+          />
+        </IntlProvider>
+      </Provider>,
+    );
+
+    expect(queryByText('Latest Strings')).toBeInTheDocument();
+    expect(queryByText('Load More')).toBeInTheDocument();
+  });
+
+  describe('mapDispatchToProps', () => {
+    describe('loadStrings', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+
+        expect(result.loadStrings).toBeDefined();
+      });
+
+      it('should dispatch loadStrings when called', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        const string = 'test';
+        result.loadStrings(string);
+
+        expect(dispatch).toHaveBeenCalledWith(loadStrings(string));
+      });
+    });
   });
 });
--- app/containers/Strings/AllStrings/tests/saga.test.js
+++ app/containers/Strings/AllStrings/tests/saga.test.js
@@ -3,13 +3,57 @@
  */
 
 /* eslint-disable redux-saga/yield-effects */
-// import { take, call, put, select } from 'redux-saga/effects';
-// import allStringsSaga from '../saga';
-
-// const generator = allStringsSaga();
+import { put, takeLatest } from 'redux-saga/effects';
+import { LOAD_STRINGS } from '../../constants';
+import { loadStringsSuccess, loadStringsError } from '../actions';
+import allStringsSaga, { fetchStrings } from '../saga';
 
 describe('allStringsSaga Saga', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  let addNewStringGenerator;
+  let fetchStringsGenerator;
+  const testStart = 0;
+
+  beforeAll(() => {
+    addNewStringGenerator = allStringsSaga();
+    fetchStringsGenerator = fetchStrings({
+      type: LOAD_STRINGS,
+      data: testStart,
+    });
+    fetchStringsGenerator.next();
+  });
+
+  it('should dispatch LOAD_STRINGS action', () => {
+    expect(addNewStringGenerator.next().value).toEqual(
+      takeLatest(LOAD_STRINGS, fetchStrings),
+    );
+  });
+
+  it('should be done on next iteration', () => {
+    expect(addNewStringGenerator.next().done).toBe(true);
+  });
+
+  it('should call api and return string if called successfully action', () => {
+    const response = {
+      data: { strings: ['test'] },
+    };
+
+    expect(fetchStringsGenerator.next(response).value).toEqual(
+      put(loadStringsSuccess(['test'])),
+    );
+  });
+
+  it('should throw error if returned error response', () => {
+    const error = {
+      response: {
+        status: 500,
+        data: {
+          message: 'Error Response',
+        },
+      },
+    };
+
+    expect(fetchStringsGenerator.throw(error).value).toEqual(
+      put(loadStringsError([expect.any(String)])),
+    );
   });
 });
