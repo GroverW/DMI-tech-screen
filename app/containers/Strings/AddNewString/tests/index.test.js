a43e90bd0b8203f54f395d844eea026ed9e2e893
GroverW <cheesemaker@gmail.com>
2020-05-12 08:41:56 -0700

    added index and saga tests to AddNewString

--- app/containers/Strings/AddNewString/tests/index.test.js
+++ app/containers/Strings/AddNewString/tests/index.test.js
@@ -9,40 +9,120 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
-// import 'jest-dom/extend-expect'; // add some helpful assertions
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
+import configureStore from '../../../../configureStore';
+import 'jest-dom/extend-expect'; // add some helpful assertions
+import { addString } from '../actions';
 
-import { AddNewString } from '../index';
+import { AddNewString, mapDispatchToProps } from '../index';
 import { DEFAULT_LOCALE } from '../../../../i18n';
 
 describe('<AddNewString />', () => {
+  let store;
+  let mockTheme;
+  let resetStatuses;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+    resetStatuses = jest.fn();
+
+    mockTheme = {
+      name: '',
+      colors: {},
+    };
+  });
+
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
-    const dispatch = jest.fn();
     render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <AddNewString dispatch={dispatch} />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <AddNewString
+            loading={false}
+            errors={false}
+            loaded={false}
+            theme={mockTheme}
+            resetStatuses={resetStatuses}
+          />
+        </IntlProvider>
+      </Provider>,
     );
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('Expect to have additional unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-
-  /**
-   * Unskip this test to use it
-   *
-   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
-   */
-  it.skip('Should render and match the snapshot', () => {
+  it('Should render and match the snapshot', () => {
     const {
       container: { firstChild },
     } = render(
-      <IntlProvider locale={DEFAULT_LOCALE}>
-        <AddNewString />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <AddNewString
+            loading={false}
+            errors={false}
+            loaded={false}
+            theme={mockTheme}
+            resetStatuses={resetStatuses}
+          />
+        </IntlProvider>
+      </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
+  });
+
+  it('Should not reset statuses if loading and error are false', () => {
+    render(
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <AddNewString
+            loading={false}
+            errors={false}
+            loaded={false}
+            theme={mockTheme}
+            resetStatuses={resetStatuses}
+          />
+        </IntlProvider>
+      </Provider>,
+    );
+    expect(resetStatuses).not.toHaveBeenCalled();
+  });
+
+  it('Should load the body content', async () => {
+    const { queryByText } = render(
+      <Provider store={store}>
+        <IntlProvider locale={DEFAULT_LOCALE}>
+          <AddNewString
+            loading={false}
+            errors={false}
+            loaded={false}
+            theme={mockTheme}
+            resetStatuses={resetStatuses}
+          />
+        </IntlProvider>
+      </Provider>,
+    );
+
+    expect(queryByText('Add a New String')).toBeInTheDocument();
+    expect(queryByText('Add String')).toBeInTheDocument();
+  });
+
+  describe('mapDispatchToProps', () => {
+    describe('addString', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+
+        expect(result.addString).toBeDefined();
+      });
+
+      it('should dispatch addString when called', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        const string = 'test';
+        result.addString(string);
+
+        expect(dispatch).toHaveBeenCalledWith(addString(string));
+      });
+    });
   });
 });
--- app/containers/Strings/AddNewString/tests/saga.test.js
+++ app/containers/Strings/AddNewString/tests/saga.test.js
@@ -3,13 +3,57 @@
  */
 
 /* eslint-disable redux-saga/yield-effects */
-// import { take, call, put, select } from 'redux-saga/effects';
-// import addNewStringSaga from '../saga';
-
-// const generator = addNewStringSaga();
+import { put, takeLatest } from 'redux-saga/effects';
+import { ADD_STRING } from '../../constants';
+import { addStringSuccess, addStringError } from '../actions';
+import addNewStringSaga, { postNewString } from '../saga';
 
 describe('addNewStringSaga Saga', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  let addNewStringGenerator;
+  let postNewStringGenerator;
+  const testString = 'test';
+
+  beforeAll(() => {
+    addNewStringGenerator = addNewStringSaga();
+    postNewStringGenerator = postNewString({
+      type: ADD_STRING,
+      data: testString,
+    });
+    postNewStringGenerator.next();
+  });
+
+  it('should dispatch ADD_STRING action', () => {
+    expect(addNewStringGenerator.next().value).toEqual(
+      takeLatest(ADD_STRING, postNewString),
+    );
+  });
+
+  it('should be done on next iteration', () => {
+    expect(addNewStringGenerator.next().done).toBe(true);
+  });
+
+  it('should call api and return string if called successfully action', () => {
+    const response = {
+      data: { string: testString },
+    };
+
+    expect(postNewStringGenerator.next(response).value).toEqual(
+      put(addStringSuccess(testString)),
+    );
+  });
+
+  it('should throw error if returned error response', () => {
+    const error = {
+      response: {
+        status: 500,
+        data: {
+          message: 'Error Response',
+        },
+      },
+    };
+
+    expect(postNewStringGenerator.throw(error).value).toEqual(
+      put(addStringError([expect.any(String)])),
+    );
   });
 });
