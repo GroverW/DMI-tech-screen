e83a77854382bc8ff9b98854ce0c8461e7f344ea
GroverW <cheesemaker@gmail.com>
2020-05-12 12:56:24 -0700

    added App tests

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
+import configureStore from '../../../configureStore';
 
 import App from '../index';
 
 const renderer = new ShallowRenderer();
 
 describe('<App />', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
   it('should render and match the snapshot', () => {
-    renderer.render(<App />);
+    renderer.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
     const renderedOutput = renderer.getRenderOutput();
     expect(renderedOutput).toMatchSnapshot();
   });
