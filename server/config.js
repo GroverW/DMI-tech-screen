c8a3d53c4a86a18054e1910c7022536a6f0fb984
GroverW <cheesemaker@gmail.com>
2020-05-12 13:06:38 -0700

    moved db to config

--- server/config.js
+++ server/config.js
@@ -2,6 +2,12 @@
 
 const RESULTS_PER_PAGE = 25;
 
+const db = new Array(115).fill(0).map((v, i) => ({
+  id: i + 1,
+  text: `This is string ${i + 1}`,
+}));
+
 module.exports = {
   RESULTS_PER_PAGE,
+  db,
 };
